d2ab2daf811fd34f43e90d6554d1431e205daf8d
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-03-13 01:54:58 +0200

    added drag and drop

--- src/components/ExtensionComponent.vue
+++ src/components/ExtensionComponent.vue
@@ -1,90 +1,104 @@
 <script setup lang="ts">
 import uniqid from "uniqid"
-import {reactive, watch} from "vue";
+import {reactive, ref, watch} from "vue";
 import IconCheckboxBlankOutline from '~icons/mdi/checkbox-blank-outline'
 import IconCheckboxMarked from '~icons/mdi/checkbox-marked'
+import draggable from "vuedraggable";
 
-  const defaultData = {
-    columns: [
-      {
-        name: "now",
-        list: [
-        ]
-      },
-      {
-        name: "next",
-        list: [
-        ]
-      },
-      {
-        name: "later",
-        list: [
-        ]
-      }
-    ]
-  } as { columns: Column[] }
 
-  const localStorageString = localStorage.getItem("toodaloo-app-data");
-  const localStorageData = localStorageString ? JSON.parse(localStorageString) : defaultData;
+const drag = ref(false);
+const defaultData = {
+  columns: [
+    {
+      name: "now",
+      list: []
+    },
+    {
+      name: "next",
+      list: []
+    },
+    {
+      name: "later",
+      list: []
+    }
+  ]
+} as { columns: Column[] }
 
-  const data = reactive(localStorageData);
+const localStorageString = localStorage.getItem("toodaloo-app-data");
+const localStorageData = localStorageString ? JSON.parse(localStorageString) : defaultData;
 
-  watch(data, (newData) => {
-    localStorage.setItem("toodaloo-app-data", JSON.stringify(newData))
-  });
+const data = reactive(localStorageData);
 
-  interface Column {
-      name: string;
-      list: {
-        id: string;
-        description: string;
-        state: "new" | "done";
-      }
+watch(data, (newData) => {
+  localStorage.setItem("toodaloo-app-data", JSON.stringify(newData))
+});
+
+interface Column {
+  name: string;
+  list: {
+    id: string;
+    description: string;
+    state: "new" | "done";
   }
+}
 
 const addItem = (event: SubmitEvent, column: Column) => {
-    const form = event.target as HTMLFormElement;
-    const formData = new FormData(event.target as HTMLFormElement);
-    const description = formData.get("description") as string;
-    console.log(description)
+  const form = event.target as HTMLFormElement;
+  const formData = new FormData(event.target as HTMLFormElement);
+  const description = formData.get("description") as string;
+  console.log(description)
 
-    column.list.push({
-      id: uniqid,
-      description,
-      state: "new"
-    });
+  column.list.push({
+    id: uniqid,
+    description,
+    state: "new"
+  });
 
-    form.reset();
-  }
+  form.reset();
+}
 
 </script>
 
 <template>
-  <div class="flex flex-row justify-center space-x-4">
+  <div class="flex flex-row justify-center space-x-5 pt-10">
     <div
         v-for="column of data.columns"
         :key="column.name"
-        class="rounded-md shadow-md bg-slate-100 py-4 px-4 w-1/4 space-y-5">
-      <div>{{ column.name }}</div>
-      <div
-           v-for="item of column.list"
-           :key="item.id"
-           class="bg-slate-50 rounded-md py-2 px-3 shadow-md flex flex-row space-x-2 items-start">
-        <button
-            class="mt-[2px]"
-            @click="item.state = item.state === 'done' ? 'new' : 'done'"
-        >
-          <IconCheckboxBlankOutline v-if="item.state !== 'done'" />
-          <IconCheckboxMarked v-else />
-        </button>
-        <span class="inline-block break-words min-w-0"
-              :class="{
+        class="rounded-md shadow-md bg-slate-100 py-4 px-4 ga w-1/4 space-y-5">
+      <div class="mb-3">{{ column.name }}</div>
+      <draggable
+          v-model="column.list"
+          group="items"
+          item-key="id"
+          class="space-y-4"
+          :class="{
+            'min-h-[40px] bg-slate-200 rounded-md py-3' : drag,
+          }"
+          @start="drag = true"
+          @end="drag = false"
+          ghost-class="ghost"
+      >
+        <template #item="{ element: item }">
+          <div
+              class="bg-slate-50 rounded-md py-2 px-3 shadow-md flex flex-row space-x-2 items-start">
+            <button
+                class="mt-[2px]"
+                @click="item.state = item.state === 'done' ? 'new' : 'done'"
+            >
+              <IconCheckboxBlankOutline v-if="item.state !== 'done'"/>
+              <IconCheckboxMarked v-else/>
+            </button>
+            <span
+                class="inline-block break-words min-w-0"
+                :class="{
                 'line-through': item.state === 'done',
         }"
-        >
+            >
            {{ item.description }}
         </span>
-      </div>
+          </div>
+        </template>
+      </draggable>
       <div class="pt-4">
         <form @submit.prevent="addItem($event, column)" class="flex flex-col justify-end">
           <input
@@ -99,3 +113,9 @@ const addItem = (event: SubmitEvent, column: Column) => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.ghost {
+  @apply bg-slate-400 opacity-40;
+}
+</style>
