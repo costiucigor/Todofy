820b1d8b6bb99ece03a85450b0d97db47b7a0588
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-03-12 23:58:20 +0200

    first commit

---
+++ src/components/ExtensionComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+  const data = {
+    columns: [
+      {
+        name: "now",
+        list: [
+          {
+            description: "ewwe"
+            state: "new",
+          },
+          {
+            description: "This is another item",
+            state: "done",
+          }
+        ]
+      }
+    ]
+  }
+
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
