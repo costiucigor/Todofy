f3e453ce2c056d9a7bc6efb735cf948cb37a4d60
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-03-14 23:52:45 +0200

    bg image

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@ import ExtensionComponent from './components/ExtensionComponent.vue'
 </script>
 
 <template>
+  <div class="bg-image">
   <extension-component />
+  </div>
 </template>
 
+<style>
+.bg-image {
+  background-image: url('/src/assets/panorama-nature-hills-moldova-near-balanesti-village-193107868.jpg');
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+}
+</style>
--- src/components/ExtensionComponent.vue
+++ src/components/ExtensionComponent.vue
@@ -5,7 +5,7 @@ import IconCheckboxBlankOutline from '~icons/mdi/checkbox-blank-outline'
 import IconCheckboxMarked from '~icons/mdi/checkbox-marked'
 import IconTrashCan from '~icons/mdi/trash-can'
 import IconEdit from '~icons/mdi/edit'
-import { format } from 'date-fns'
+import {format} from 'date-fns'
 import draggable from "vuedraggable";
 
 const time = ref('')
@@ -21,11 +21,11 @@ const drag = ref(false);
 const defaultData = {
   columns: [
     {
-      name: "Now",
+      name: "Next",
       list: []
     },
     {
-      name: "Next",
+      name: "Latt",
       list: []
     },
     {
@@ -37,7 +37,7 @@ const defaultData = {
 
 const data = reactive(defaultData);
 
-if (chrome?.storage){
+if (chrome?.storage) {
   chrome.storage.sync.get(["key"], (result) => {
     const storeData = result["key"];
     if (storeData) {
@@ -53,7 +53,7 @@ if (chrome?.storage){
 
 watch(data, (newData) => {
   if (chrome?.storage) {
-    chrome.storage.sync.set({key: toRaw(data) }, () => {
+    chrome.storage.sync.set({key: toRaw(data)}, () => {
     });
   } else {
     localStorage.setItem("todofy-data", JSON.stringify(newData))
@@ -103,110 +103,117 @@ const editItem = (event: Event, column: Column, listIndex: number) => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center mt-10 text-2xl font-bold text-gray-800">
-    <div class="mb-2">{{ time }}</div>
-    <div class="text-lg font-normal">{{ date }}</div>
-  </div>
-  <div class="flex flex-row justify-center space-x-5 pt-10">
-    <div
-        v-for="column of data.columns"
-        :key="column.name"
-        class="rounded-md mt-40 shadow-md bg-slate-300 py-4 px-4 ga w-1/4 space-y-5">
-      <div class="mb-3 ml-40 text-xl font-bold justify-center">{{ column.name }}</div>
-      <draggable
-          v-model="column.list"
-          group="items"
-          item-key="id"
-          class="space-y-4"
-          :class="{
+    <div class="flex flex-col items-center mt-10 text-2xl font-bold text-gray-800">
+      <div class="mb-2">{{ time }}</div>
+      <div class="text-lg font-normal">{{ date }}</div>
+    </div>
+    <div class="flex flex-row justify-center space-x-5 pt-10">
+      <div
+          v-for="column of data.columns"
+          :key="column.name"
+          class="rounded-md mt-40 shadow-md bg-slate-300 py-4 px-4 ga w-1/4 space-y-5">
+        <div class="mb-3 ml-40 text-xl font-bold justify-center">{{ column.name }}</div>
+        <draggable
+            v-model="column.list"
+            group="items"
+            item-key="id"
+            class="space-y-4"
+            :class="{
             'min-h-[40px] bg-slate-200 rounded-md py-3' : drag,
           }"
-          @start="drag = true"
-          @end="drag = false"
-          ghost-class="ghost"
-      >
-        <template #item="{ element: item, index: listIndex }">
-          <div
-              class="group bg-slate-50 rounded-md py-2 px-3 shadow-md flex flex-row space-x-2 items-start relative"
-          >
-            <template v-if="item.editing">
-              <form
-                  v-if="item.editing"
-                  @submit.prevent="editItem($event, column, listIndex)"
-                  class="flex flex-col w-full"
-              >
-                <input
-                    type="text"
-                    value="test"
-                    name="description"
-                    :value="item.description"
-                    class="w-full px-3 py-2 rounded-md mb-2"
-                    autocomplete="off"
-                    required
-                />
-                <div class="flex justify-between">
-                  <button type="button"
-                          @click="item.editing = false"
-                          class="bg-red-700 mb-4 rounded-md text-white text-l hover:bg-red-500 font-heavy w-20 transition duration-300 transform hover:-translate-y-1 hover:scale-110 leading-extra-loose"
-                  >
-                    Cancel
-                  </button>
-                  <button class="bg-green-700 mb-4 rounded-md text-white text-l hover:bg-green-500 font-heavy w-20 transition duration-300 transform hover:-translate-y-1 hover:scale-110 leading-extra-loose">Save</button>
+            @start="drag = true"
+            @end="drag = false"
+            ghost-class="ghost"
+        >
+          <template #item="{ element: item, index: listIndex }">
+            <div
+                class="group bg-slate-50 rounded-md py-2 px-3 shadow-md flex flex-row space-x-2 items-start relative"
+            >
+              <template v-if="item.editing">
+                <form
+                    v-if="item.editing"
+                    @submit.prevent="editItem($event, column, listIndex)"
+                    class="flex flex-col w-full"
+                >
+                  <input
+                      type="text"
+                      value="test"
+                      name="description"
+                      :value="item.description"
+                      class="w-full px-3 py-2 rounded-md mb-2"
+                      autocomplete="off"
+                      required
+                  />
+                  <div class="flex justify-between">
+                    <button type="button"
+                            @click="item.editing = false"
+                            class="bg-red-700 mb-4 rounded-md text-white text-l hover:bg-red-500 font-heavy w-20 transition duration-300 transform hover:-translate-y-1 hover:scale-110 leading-extra-loose"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                        class="bg-green-700 mb-4 rounded-md text-white text-l hover:bg-green-500 font-heavy w-20 transition duration-300 transform hover:-translate-y-1 hover:scale-110 leading-extra-loose">
+                      Save
+                    </button>
+                  </div>
+                </form>
+              </template>
+              <template v-else>
+                <div
+                    class="absolute hidden right-0 bottom-0 p-3 bg-slate-50 opacity-90 max-h-full space-x-3 flex-row group-hover:flex"
+                >
+                  <div>
+                    <button @click="item.editing = true">
+                      <IconEdit/>
+                    </button>
+                    <button @click="deleteItem(column, listIndex)">
+                      <IconTrashCan class="text-red-700"/>
+                    </button>
+                  </div>
                 </div>
-              </form>
-            </template>
-            <template v-else>
-              <div
-                  class="absolute hidden right-0 bottom-0 p-3 bg-slate-50 opacity-90 max-h-full space-x-3 flex-row group-hover:flex"
-              >
-                <div>
-                  <button @click="item.editing = true">
-                    <IconEdit/>
-                  </button>
-                  <button @click="deleteItem(column, listIndex)">
-                    <IconTrashCan class="text-red-700"/>
-                  </button>
-                </div>
-              </div>
-              <button
-                  class="mt-[2px]"
-                  @click="item.state = item.state === 'done' ? 'new' : 'done'"
-              >
-                <IconCheckboxBlankOutline v-if="item.state !== 'done'"/>
-                <IconCheckboxMarked v-else/>
-              </button>
-              <span
-                  class="inline-block break-words min-w-0"
-                  :class="{
+                <button
+                    class="mt-[2px]"
+                    @click="item.state = item.state === 'done' ? 'new' : 'done'"
+                >
+                  <IconCheckboxBlankOutline v-if="item.state !== 'done'"/>
+                  <IconCheckboxMarked v-else/>
+                </button>
+                <span
+                    class="inline-block break-words min-w-0"
+                    :class="{
                 'line-through': item.state === 'done',
         }"
-              >
+                >
            {{ item.description }}
         </span>
-            </template>
-          </div>
-        </template>
-      </draggable>
-      <div class="pt-4">
-        <form @submit.prevent="addItem($event, column)" class="flex flex-col justify-end">
-          <input
-              type="text"
-              name="description"
-              class="w-full px-3 py-2 rounded-md mb-2"
-              autocomplete="off"
-              required
-          />
-          <div class="flex justify-center">
-            <button class="bg-sky-700 mb-4 rounded-md text-white text-l hover:bg-sky-500 font-heavy w-20 transition duration-300 transform hover:-translate-y-1 hover:scale-110 leading-extra-loose">Add</button>
-          </div>
-        </form>
+              </template>
+            </div>
+          </template>
+        </draggable>
+        <div class="pt-4">
+          <form @submit.prevent="addItem($event, column)" class="flex flex-col justify-end">
+            <input
+                type="text"
+                name="description"
+                class="w-full px-3 py-2 rounded-md mb-2"
+                autocomplete="off"
+                required
+            />
+            <div class="flex justify-center">
+              <button
+                  class="bg-sky-700 mb-4 rounded-md text-white text-l hover:bg-sky-500 font-heavy w-20 transition duration-300 transform hover:-translate-y-1 hover:scale-110 leading-extra-loose">
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 .ghost {
   @apply bg-slate-400 opacity-40;
 }
+
 </style>
