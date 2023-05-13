575500fdca0be02535f96d82ae3441ab250b0b31
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-05-13 10:08:34 +0300

    partially fixed bugs

--- src/components/LinksComponent.vue
+++ src/components/LinksComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import {computed, ref} from "vue";
 
 interface Link {
   name: string;
@@ -33,38 +33,50 @@ const addLink = () => {
   if (showAddLinkForm.value) {
     showAddLinkForm.value = false;
   }
+
+  saveLinksToStorage();
 };
 
 const deleteLink = (index: number) => {
   links.value.splice(index, 1);
+  saveLinksToStorage();
 };
 
-const onFormSubmit = async (event: Event) => {
-  event.preventDefault(); // prevent the default form submission behavior
+const updateLink = (index: number) => {
+  const linkToUpdate = links.value[index];
+  // perform some update operation on linkToUpdate, for example:
+  linkToUpdate.name = editName.value;
+  linkToUpdate.url = editUrl.value;
+  saveLinksToStorage();
+};
+
+const onFormSubmit = (event: Event) => {
+  event.preventDefault();
 
   if (!newLinkName.value || !newLinkUrl.value) {
     return;
   }
 
   if (showEditLinkForm.value >= 0) {
-    await updateLink(showEditLinkForm.value); // wait for the link to be updated
-    showEditLinkForm.value = -1; // hide the form
+    updateLink(showEditLinkForm.value);
   } else {
-    await addLink(); // wait for the new link to be added
-    showAddLinkForm.value = false; // hide the form
+    links.value.push({
+      name: newLinkName.value,
+      url: newLinkUrl.value
+    });
+    saveLinksToStorage();
   }
+
+  newLinkName.value = "";
+  newLinkUrl.value = "";
+  showAddLinkForm.value = false;
+  showEditLinkForm.value = -1;
 };
 
 const onEditFormSubmit = (event: Event, index: number) => {
   event.preventDefault(); // prevent the default form submission behavior
 
-  if (!editName.value || !editUrl.value) {
-    return;
-  }
-
-  links.value[index].name = editName.value;
-  links.value[index].url = editUrl.value;
-
+  updateLink(index);
   showEditLinkForm.value = -1; // hide the form
 };
 
@@ -75,42 +87,48 @@ const editLink = (index: number) => {
   showEditLinkForm.value = index;
 };
 
-const cancelEdit = () => {
-  showEditLinkForm.value = -1;
+const saveLinksToStorage = () => {
+  const serializedLinks = JSON.stringify(links.value);
+  localStorage.setItem("links", serializedLinks);
 };
+
+const currentLinkName = computed(() => {
+  return showEditLinkForm.value >= 0 ? editName.value : newLinkName.value;
+});
 
 const newLinkName = ref("");
 const newLinkUrl = ref("");
+
 </script>
 
 <template>
-    <div class="relative">
-      <div class="relative ml-4">
-        <button
-            type="button"
-            @click="showDropDown = !showDropDown"
-            :class="{'text-black border-black': showDropDown, 'text-white': !showDropDown}"
-            class="px-2 py-1 rounded border"
-        >
-          Links
-        </button>
-        <ul v-show="showDropDown" @click.away.stop="showDropDown = false" class="absolute z-10 bg-white py-2 mt-2 w-64 rounded-lg shadow-lg" style="right: 0;">
-          <li v-for="(link, index) in links" :key="index" class="px-4 py-2">
-            <a :href="link.url" target="_blank" rel="noopener noreferrer" class="text-blue-500">{{ link.name }}</a>
-            <button @click.stop="editLink(index)" class="ml-2 text-gray-500 hover:text-gray-700">Edit</button>
-            <button @click.stop="deleteLink(index)" class="ml-2 text-gray-500 hover:text-gray-700">Delete</button>
-          </li>
-          <li class="border-t border-gray-200 px-4 py-2">
-            <button
-                type="button"
-                @click.stop="showAddLinkForm = !showAddLinkForm"
-                class="text-gray-500 hover:text-gray-700"
-            >
-              + New Link
-            </button>
-          </li>
-        </ul>
-      </div>
+  <div class="relative">
+    <div class="relative ml-4">
+      <button
+          type="button"
+          @click="showDropDown = !showDropDown"
+          :class="{'text-black border-black': showDropDown, 'text-white': !showDropDown}"
+          class="px-2 py-1 rounded border"
+      >
+        Links
+      </button>
+      <ul v-show="showDropDown" @click.away.stop="showDropDown = false" class="absolute z-10 bg-white py-2 mt-2 w-64 rounded-lg shadow-lg" style="right: 0;">
+        <li v-for="(link, index) in links" :key="index" class="px-4 py-2">
+          <a :href="link.url" target="_blank" rel="noopener noreferrer" class="text-blue-500">{{ link.name }}</a>
+          <button @click.stop="editLink(index)" class="ml-2 text-gray-500 hover:text-gray-700">Edit</button>
+          <button @click.stop="deleteLink(index)" class="ml-2 text-gray-500 hover:text-gray-700">Delete</button>
+        </li>
+        <li class="border-t border-gray-200 px-4 py-2">
+          <button
+              type="button"
+              @click.stop="showAddLinkForm = !showAddLinkForm"
+              class="text-gray-500 hover:text-gray-700"
+          >
+            + New Link
+          </button>
+        </li>
+      </ul>
+    </div>
 
     <div class="relative">
       <form
@@ -119,41 +137,45 @@ const newLinkUrl = ref("");
           class="absolute top-0 right-0 mt-2 mr-2 w-64 bg-white rounded-lg p-4 shadow-lg z-10"
       >
         <div class="mb-4">
-          <label class="block text-gray-700 font-bold mb-2" for="new-link-name">Name:</label>
-          <input
-              class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="new-link-name"
-              v-model="newLinkName"
-              type="text"
-              required
-              @click.stop
-          />
-        </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 font-bold mb-2" for="new-link-url">URL:</label>
-          <input
-              class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="new-link-url"
-              v-model="newLinkUrl"
-              type="url"
-              required
-              @click.stop
-          />
-        </div>
-        <div class="flex justify-end">
-          <button
-              class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-              type="submit"
-          >
-            {{ showEditLinkForm >= 0 ? 'Save' : 'Add Link' }}
-          </button>
-          <button
-              class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors ml-4"
-              type="button"
-              @click="showAddLinkForm = false; showEditLinkForm = -1"
-          >
-            Cancel
-          </button>
+          <div v-if="showEditLinkForm >= 0 || showAddLinkForm" class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="edit-link-name">Name:</label>
+            <input
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="edit-link-name"
+                :value="currentLinkName"
+                @input="showEditLinkForm >= 0 ? editName = $event.target.value : newLinkName = $event.target.value"
+                type="text"
+                required
+                @click.stop
+            />
+          </div>
+          <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="edit-link-url">URL:</label>
+            <input
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="edit-link-url"
+                :value="newLinkUrl || editUrl"
+                type="text"
+                required
+                @click.stop
+            />
+          </div>
+          <div class="flex justify-end">
+            <button
+                type="button"
+                @click.stop="showAddLinkForm = false; showEditLinkForm = -1"
+                class="bg-white text-gray-700 hover:bg-gray-100 py-2 px-4 border border-gray-300 rounded shadow mr-2"
+            >
+              Cancel
+            </button>
+            <button
+                type="submit"
+                :disabled="!newLinkName && !editName || !newLinkUrl && !editUrl"
+                class="bg-blue-500 text-white py-2 px-4 rounded shadow"
+            >
+              {{ showEditLinkForm >= 0 ? 'Save' : 'Add' }}
+            </button>
+          </div>
         </div>
       </form>
     </div>
